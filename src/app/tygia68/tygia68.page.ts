a40c8d927620048952c2275adf966f3a038f8d79
DangKhoa <vodangkhoa835@gmail.com>
2024-04-09 16:40:12 +0700

    update

--- src/app/tygia68/tygia68.page.ts
+++ src/app/tygia68/tygia68.page.ts
@@ -1,20 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+import { addIcons } from 'ionicons';
+import { notifications } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tygia68',
   templateUrl: './tygia68.page.html',
   styleUrls: ['./tygia68.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule]
 })
-export class Tygia68Page implements OnInit {
+export class Tygia68Page  {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() { 
+    addIcons({notifications})
   }
+
+  
 
 }
